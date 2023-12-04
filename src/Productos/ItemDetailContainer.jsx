706224abcb4325efc3f3d508818a9af9770fb21a
BrunoAdriel <109391716+BrunoAdriel@users.noreply.github.com>
2023-12-04 18:37:04 -0300

    cambio de los productos a un .json

--- src/Productos/ItemDetailContainer.jsx
+++ src/Productos/ItemDetailContainer.jsx
@@ -1,38 +1,21 @@
-import React, { useState } from 'react'
-import Card from './Card'
+import React, { useState, useEffect } from 'react';
+
 import ItemDetail from './ItemDetail';
-import NorthFaceBlanca from "../imgs/NortFaceBlanca.jpg";
-import CamperaNikeNegra from "../imgs/CamperaNikeNegra.jpg"
-import CamperaNikePuffer from "../imgs/CamperaNikePuffer.jpg"
-import CamperaAdidas from "../imgs/CamperaAdidas.jpg"
-import CamperonAdidas from "../imgs/CamperonAdidas.jpg"
-import CamperonNorthFace from "../imgs/CamperonNorthFace.jpg"
-import CamperaNorthFaceBySuprime from "../imgs/CamperaNorthFaceBySupreme.jpg"
-import CamperaNikeRunning from "../imgs/CamperaNikeRunning.jpg"
-import CamperaAdidasRunning from "../imgs/CamperaAdidasRunning.jpg"
+import ProductosJson from "../Components/ProductosJson/Productos.json"; 
+
+
 
 
 
 
 function ItemDetailContainer() {
 
-    const prods =[
-        {id: 1, nombre:"Camepera Nike", descripcion: "Lorem8", marca: "Nike", stock:15, img: CamperaNikeNegra },
-        {id: 2, nombre:"Campera Adidas", descripcion: "Lorem8", marca: "Adidas", stock:16, img: CamperaAdidas }, 
-        {id: 3, nombre:"Campera Nike Puffer", descripcion:"Campera Nike Puffer Lorem8", marca: "Nike", stock: 14, img: CamperaNikePuffer },
-        {id: 4, nombre:"Campera North Face", descripcion: "Lorem8", marca: "North Face", stock: 10, img: NorthFaceBlanca },
-        {id: 5, nombre:"Campera Adidas", descripcion:"Campera Adidas Lorem8", marca:"Adidas", stock: 15, img: CamperonAdidas },
-        {id: 6, nombre:"Camperon North Face", descripcion:"Camperon North Face Lorem8", marca:"North Face", stock: 7, img: CamperonNorthFace },
-        {id: 7, nombre:"Campera North Face By Suprime", descripcion:"Campera North Face by suprime Lorem8", marca:"North Face", stock: 9, img: CamperaNorthFaceBySuprime },
-        {id: 8, nombre:"Campera Nike Running", descripcion:"Campera Nike running Lorem8", marca: "Nike", stock: 17, img: CamperaNikeRunning },
-        {id: 9, nombre:"Campera Adidas Running", descripcion:"Camperon Adidas Running Lorem8", marca:"Adidas", stock: 20, img: CamperaAdidasRunning}
-    ]
 
-    // const [list, setList] = useState([])
+
 
     return (
         <>
-            <ItemDetail prods={prods}/>
+            {/* <ItemDetail prods={prods}/> */}
 
         </> 
     )
